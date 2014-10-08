74182f7fa5f995ba2049cd12de1b9036956fd3fd
rahil <rahil@blinkbox.com>
2014-10-08 14:49:50 +0100

    Added schemas for gift redeemed messages

---
+++ src/main/scala/com/blinkbox/books/schemas/events/gifting/v2/Schema.scala
@@ -0,0 +1,17 @@
+package com.blinkbox.books.schemas.events.gifting.v2
+
+import com.blinkbox.books.messaging.{EventBody, MediaType, JsonEventBody}
+import org.joda.time.DateTime
+
+case class Campaign(id: Int, name: String)
+case class RedemptionDetails(actionDetails: Map[String, String])
+
+object Gift {
+  case class Redeemed(campaign: Campaign, redemptionDetails: RedemptionDetails, userId: Int, redeemedAt: DateTime)
+
+  implicit object Redeemed extends JsonEventBody[Redeemed] {
+    override val jsonMediaType = MediaType("application/vnd.blinkbox.books.events.gifting.redeemed.v2+json")
+    def unapply(body: EventBody): Option[(Campaign, RedemptionDetails, Int, DateTime)] =
+      JsonEventBody.unapply[Redeemed](body).flatMap(Redeemed.unapply)
+  }
+}
