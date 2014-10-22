dc055d23f1a259e3027c1f9a67fa635f253ed378
csargent <chriss@blinkbox.com>
2014-10-22 12:51:35 +0100

    Removed intermediate case class for gift details

--- src/main/scala/com/blinkbox/books/schemas/events/gifting/v2/Schema.scala
+++ src/main/scala/com/blinkbox/books/schemas/events/gifting/v2/Schema.scala
@@ -4,14 +4,13 @@ import com.blinkbox.books.messaging.{EventBody, MediaType, JsonEventBody}
 import org.joda.time.DateTime
 
 case class Campaign(id: Int, name: String)
-case class GiftDetails(giftDetails: Map[String, String])
 
 object Gift {
-  case class Redeemed(campaign: Campaign, giftDetails: GiftDetails, userId: Int, redeemedAt: DateTime)
+  case class Redeemed(campaign: Campaign, giftDetails: Map[String, String], userId: Int, redeemedAt: DateTime)
 
   implicit object Redeemed extends JsonEventBody[Redeemed] {
     override val jsonMediaType = MediaType("application/vnd.blinkbox.books.events.gifting.redeemed.v2+json")
-    def unapply(body: EventBody): Option[(Campaign, GiftDetails, Int, DateTime)] =
+    def unapply(body: EventBody): Option[(Campaign, Map[String, String], Int, DateTime)] =
       JsonEventBody.unapply[Redeemed](body).flatMap(Redeemed.unapply)
   }
 }
